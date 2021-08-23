48a1201310c4026246d1d5f38c5bfaa23695900d
Štěpán Mikéska <haroldvemeno@email.cz>
2021-08-23 15:32:02 +0200

    ~~~

--- tripleneg.v
+++ tripleneg.v
@@ -1,10 +1,79 @@
-Theorem not_not_not : forall P, ~~~P -> ~P.
+Require Import Setoid.
+Require Import Classical.
+
+Theorem not_not_not : forall P, ~~~P <-> ~P.
 Proof.
+  split.
   intros.
+  unfold not in *.
   intro.
-  unfold not in H.
   apply H.
   intros.
-  apply H1 in H0.
-  assumption.
+  apply H1.
+  apply H0.
+  unfold not.
+  intros.
+  apply (H0 H).
+Qed.
+
+Theorem wierd : forall P, ~(~P /\ ~~P).
+Proof.
+  intros P H.
+  destruct H as [A B].
+  apply (B A).
+Qed.
+
+Theorem www : forall P Q, ~(~P /\ ~Q) <-> ~~(~~P \/ ~~Q).
+  Proof.
+  intros.
+  split.
+
+  intro And.
+  unfold not in *.
+  intro Or.
+  apply And.
+  split;
+    intro aPQ;
+    apply Or;
+    [left | right];
+    intro nPQ;
+    apply (nPQ aPQ).
+  intro Or.
+  intro And.
+  unfold not in *.
+  apply Or.
+  intro Or0.
+  destruct Or0 as [Or1 | Or1];
+    apply Or1;
+    apply And.
+Qed.
+
+
+Theorem wierder : forall P, ~~(~P \/ ~~P).
+Proof.
+  intros P.                    
+  unfold not at 1 2 4 5.
+  rewrite <- not_not_not in |- *.
+  apply www.
+  unfold not.
+  intro And.
+  destruct And as [nnP nP].
+  apply (nnP nP).
+Qed.
+
+Theorem PP : forall P, ((~P)->~~P)->~~P.
+Proof.
+  intros.
+  unfold not in *.
+  intro.
+  apply (H H0 H0).
+Qed.
+
+Theorem PPP : forall P, ((~P)->P)->P.
+Proof.
+  intros.
+  elim (classic P).
+  intros.
+  apply H0.
+  apply H.
 Qed.
